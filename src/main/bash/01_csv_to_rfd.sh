f550f25ac3543a53698d39140705ebc307c5d743
gezever <gezever@skynet.be>
2023-11-07 17:32:27 +0100

    refactor source csv

--- src/main/bash/01_csv_to_rfd.sh
+++ src/main/bash/01_csv_to_rfd.sh
@@ -4,3 +4,4 @@
 # to jsonld, /tmp/sommatie_stoffen.jsonld
 Rscript ../R/csv_to_json.R
 
+shacl v --shapes ../resources/be/vlaanderen/omgeving/data/id/ontology/sommatie-stoffen-ap-constraints/sommatie-stoffen-ap-constraints.ttl --data ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/sommatie_stoffen/sommatie_stoffen.ttl
