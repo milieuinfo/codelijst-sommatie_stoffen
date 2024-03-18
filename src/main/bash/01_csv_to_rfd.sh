a6f80fd175ed5b86b599c4c9e084e9333a56e451
gezever <gezever@skynet.be>
2024-03-18 17:20:59 +0100

    javascript

--- src/main/bash/01_csv_to_rfd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Transform csv, ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/sommatie_stoffen/sommatie_stoffen.csv
-# to jsonld, /tmp/sommatie_stoffen.jsonld
-Rscript ../R/csv_to_json.R
-
-shacl v --shapes ../resources/be/vlaanderen/omgeving/data/id/ontology/sommatie-stoffen-ap-constraints/sommatie-stoffen-ap-constraints.ttl --data ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/sommatie_stoffen/sommatie_stoffen.ttl
